ad776a08174b5b4aa0f0f20ec19c48c1e13771df
limonada77 <retolimo794@gmail.com>
2026-09-08 18:38:19 -0300

    DarkTV Celular: landscape compacto e responsivo

--- android-mobile/app/src/main/java/com/darktv/mobile/MainActivity.java
+++ android-mobile/app/src/main/java/com/darktv/mobile/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
 
